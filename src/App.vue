bc309d390c4cc2a4ec0b4bc2adfa8bfc02fb3f3d
ghostpvo <roadsteer9@gmail.com>
2020-03-27 03:47:09 +0200

    fonts + scss structure

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+  @import 'assets/scss/main.scss';
 </style>
---
+++ src/components/Elements/Icon/Icon.vue
@@ -0,0 +1,49 @@
+<template>
+  <svg
+    :class="iconId"
+    class="icon"
+    viewBox="0 0 100 100"
+  >
+    <use :xlink:href="getSprite(iconId)">
+      {{ tittle }}
+    </use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    tittle: {
+      type: String,
+      default: ''
+    },
+    isActive: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    iconId () {
+      if (this.isActive) {
+        return this.icon + '-active'
+      } else {
+        return this.icon
+      }
+    }
+  },
+  methods: {
+    getSprite (type) {
+      return `/static/sprite/sprite.svg#${type}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "icon";
+</style>
